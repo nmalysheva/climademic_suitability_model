ab5d3eecc3839c057b43301ae9ad3088f46033b1
nmalysheva <51414561+nmalysheva@users.noreply.github.com>
2026-08-11 13:52:16 +0200

    config json format

--- config.py
+++ config.py
@@ -1,26 +1,19 @@
 from pathlib import Path
+import json
 
 PROJECT_ROOT = Path(__file__).resolve().parents[0] # set .../Climademic_Suitability_mdel/ a project root
+CONFIG_PATH = PROJECT_ROOT / "config.json"
+def load_config():
+    with open(CONFIG_PATH, "r") as file:
+        config = json.load(file)
 
-CONFIG = {
-    "paths": {
-        "artifacts": PROJECT_ROOT / "artifacts",
-        "raw_data": {
-            "compendium": PROJECT_ROOT / "artifacts/data_raw/Compendium/aegypti_albopictus.csv", #Kraemer's global compendium dataset
-            "climate": PROJECT_ROOT / "artifacts/data_raw/ERA5/era5_new/era_5_combined.nc", #Path to thr ERA5 files
-            "land_use": PROJECT_ROOT / "artifacts/data_raw/HILDA+",
-            "population": PROJECT_ROOT / "artifacts/data_raw/GHSL_POP",
-            "gmod_inaturalist": PROJECT_ROOT / "artifacts/data_raw/GMOD/inaturalistpoints.csv",
-            "gmod_mosquito_alert": PROJECT_ROOT / "artifacts/data_raw/GMOD/mosquitoAlert.csv"},
-        "processed_data":{
-            "training_dataset": PROJECT_ROOT / "artifacts/processed_data/training_dataset/training_dataset.csv",
-            "global_dataset":PROJECT_ROOT / "artifacts/processed_data/global_dataset/",
-            "climate_worldwide": PROJECT_ROOT / "artifacts/processed_data/global_dataset/climate/",
-            "population_worldwide":PROJECT_ROOT / "artifacts/processed_data/global_dataset/population/",
-            "land_use_worldwide": PROJECT_ROOT / "artifacts/processed_data/global_dataset/land_use/",
-            "gmod_dataset":PROJECT_ROOT / "artifacts/processed_data/GMOD_dataset/"},
-        "model_parameters":{
-            "models_directory": PROJECT_ROOT / "artifacts/models/"},
-        "predictions": PROJECT_ROOT / "artifacts/predictions/"
-    }
-}
+    config["paths"] = resolve_paths(config["paths"])
+    return config
+
+
+def resolve_paths(obj):
+    if isinstance(obj, dict):
+        return {k: resolve_paths(v) for k, v in obj.items()}
+    if isinstance(obj, str):
+        return PROJECT_ROOT / obj
+    return obj
